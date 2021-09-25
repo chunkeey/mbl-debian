4092e047baca7009df487e35fbec24ce74d6f895
Christian Lamparter <chunkeey@gmail.com>
2021-09-25 21:54:58 +0200

    kernel: Update to 5.15-rc4
     - Proper NTFS write support in kernel.
     - CIFS+SMBv3 in kernel.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- build-kernel.sh
+++ build-kernel.sh
@@ -13,7 +13,7 @@ DTS_DIR=dts
 DTS_MBL=dts/wd-mybooklive.dts
 DTB_MBL=dts/wd-mybooklive.dtb
 LINUX_DIR=linux
-LINUX_VER=v5.14-rc2
+LINUX_VER=v5.15-rc4
 
 # This "cached-linux" serves as a local cache for a unmodified linux.git
 LINUX_LOCAL="cached-linux"
