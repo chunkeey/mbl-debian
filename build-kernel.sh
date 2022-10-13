5f8221132a0253f9fcb8886d57279934533c017c
Christian Lamparter <chunkeey@gmail.com>
2022-10-13 10:30:09 +0200

    kernel: Update to 6.0
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- build-kernel.sh
+++ build-kernel.sh
@@ -13,7 +13,7 @@ DTS_DIR=dts
 DTS_MBL=dts/wd-mybooklive.dts
 DTB_MBL=dts/wd-mybooklive.dtb
 LINUX_DIR=linux
-LINUX_VER=${1:-v6.0-rc4}
+LINUX_VER=${1:-v6.0}
 LINUX_SV="$(echo $LINUX_VER | cut -d. -f-2)"
 
 # This "cached-linux" serves as a local cache for a unmodified linux.git
